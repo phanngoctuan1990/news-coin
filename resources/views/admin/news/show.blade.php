74fcf6ffc5cfc4d94cb7acd743e8e2f91730a158
asiantech <asiantech@Huy-Le-Q.net.fpt>
2017-12-16 12:51:21 +0700

    update html

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -3,13 +3,12 @@
 @section('content')
 <section class="content-header">
     <h1>
-        Create News
-        <small>advanced tables</small>
+        Bài viết
     </h1>
     <ol class="breadcrumb">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">News</a></li>
-        <li class="active">Create</li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Bài viết</a></li>
+        <li class="active">Chi tiết bài viết</li>
     </ol>
 </section>
 
@@ -17,21 +16,18 @@
     <div class="row">
         <div class="col-md-12">
             <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Quick Example</h3>
-                </div>
                 <form role="form" action="{{ route('admin.news.update', $new->id) }}" method="POST" enctype="multipart/form-data">
                     {{ csrf_field() }}
                     {{ method_field('PUT') }}
                     <div class="box-body">
                         <div class="form-group">
                             <label>Tiêu đề</label>
-                            <input type="text" name="title" value="{{ $new->title }}" class="form-control">
+                            <input type="text" name="title" value="{{ $new->title }}" class="form-control" placeholder="Nhập tiêu đề bài viết">
                         </div>
                         <!-- wysiwyg -->
                         <div class="form-group">
                             <label>Nội dung</label>
-                            <textarea class="textarea" name="content" placeholder="Place some text here" style="widt 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+                            <textarea class="textarea" name="content" placeholder="Nhập nội dung bài viết" style="widt 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
                                 {{ $new->content }}
                             </textarea>
                         </div>
