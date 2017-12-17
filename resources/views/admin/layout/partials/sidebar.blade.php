e5cdd0c55c0314e2cef100bfb62ec3a09fafb863
Ngoc Tuan <phanngoctuan1990@gmail.com>
2017-12-17 11:00:37 +0700

    Set active on sidebar

--- resources/views/admin/layout/partials/sidebar.blade.php
+++ resources/views/admin/layout/partials/sidebar.blade.php
@@ -4,10 +4,10 @@
     <section class="sidebar">
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active">
+            <li class="{{ areActiveRoutes(['admin.dashboard']) }}">
                 <a href="{{ route('admin.dashboard') }}"><span>Dashboard</span></a>
             </li>
-            <li class="treeview">
+            <li class="treeview {{ areActiveRoutes(['admin.user.index', 'admin.user.create']) }}">
                 <a href="#">
                     <span>User</span>
                     <span class="pull-right-container">
@@ -19,7 +19,7 @@
                     <li><a href="{{ route('admin.user.create') }}"><i class="fa fa-circle-o"></i> Create</a></li>
                 </ul>
             </li>
-            <li class="treeview">
+            <li class="treeview {{ areActiveRoutes(['admin.news.index', 'admin.news.create']) }}">
                 <a href="#">
                     <span>News</span>
                     <span class="pull-right-container">
@@ -31,7 +31,7 @@
                     <li><a href="{{ route('admin.news.create') }}"><i class="fa fa-circle-o"></i> Create</a></li>
                 </ul>
             </li>
-            <li class="treeview">
+            <li class="treeview {{ areActiveRoutes(['admin.coin.index', 'admin.coin.create']) }}">
                 <a href="#">
                     <span>Coin</span>
                     <span class="pull-right-container">
