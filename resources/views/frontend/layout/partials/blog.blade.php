8b06acf690cf45a71e20878b076431e467cfd59a
Ngoc Tuan <phanngoctuan1990@gmail.com>
2017-12-29 17:37:58 +0700

    Update list coins in hoame page

---
+++ resources/views/frontend/layout/partials/blog.blade.php
@@ -0,0 +1,72 @@
+<div id="blog" class="section md-padding bg-grey">
+
+    <!-- Container -->
+    <div class="container">
+
+        <!-- Row -->
+        <div class="row">
+
+            <!-- Section header -->
+            <div class="section-header text-center">
+                <h2 class="title">Recents news</h2>
+            </div>
+            <!-- /Section header -->
+
+            <!-- blog -->
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="./images/front/blog1.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim
+                            nec.
+                        </p>
+                        <a href="news.html">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /blog -->
+
+            <!-- blog -->
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="./images/front/blog2.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim
+                            nec.
+                        </p>
+                        <a href="news.html">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /blog -->
+
+            <!-- blog -->
+            <div class="col-md-4">
+                <div class="blog">
+                    <div class="blog-img">
+                        <img class="img-responsive" src="./images/front/blog3.jpg" alt="">
+                    </div>
+                    <div class="blog-content">
+                        <h3>Molestie at elementum eu facilisis sed odio</h3>
+                        <p>Nec feugiat nisl pretium fusce id velit ut tortor pretium. Nisl purus in mollis nunc sed. Nunc non blandit massa enim
+                            nec.
+                        </p>
+                        <a href="news.html">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <!-- /blog -->
+
+        </div>
+        <!-- /Row -->
+
+    </div>
+    <!-- /Container -->
+
+</div>
