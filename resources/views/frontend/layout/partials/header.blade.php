905823781e73fcc8e0c89651e10231af95b09e88
hoa.nguyen <hoa.nguyen@asiantech.vn>
2018-01-07 17:11:48 +0700

    replace index.html

--- resources/views/frontend/layout/partials/header.blade.php
+++ resources/views/frontend/layout/partials/header.blade.php
@@ -7,7 +7,7 @@
             <div class="navbar-header">
                 <!-- Logo -->
                 <div class="navbar-brand">
-                    <a href="index.html">
+                    <a href="http://idautu.net/">
                         <img class="logo" src="{{ '/images/front/logo.png' }}" alt="logo">
                         <img class="logo-alt" src="{{ '/images/front/logo-alt.png' }}" alt="logo">
                     </a>
