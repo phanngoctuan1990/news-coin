188c5ba800ccbf64f8aff0820f63c67c575844b7
Ngoc Tuan <phanngoctuan1990@gmail.com>
2018-01-23 13:40:37 +0700

    Update code

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -45,7 +45,7 @@ $(function() {
   });
 
   $(document).ready(function() {
-      $('img').addClass('img-responsive');
-      $('#news-image').removeClass('img-responsive');
+      $('.blog-content').find('img').addClass('img-responsive');
+      // $('#news-image').removeClass('img-responsive');
   });
 });
